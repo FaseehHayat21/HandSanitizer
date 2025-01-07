eab0250f36828c81b2e5af9e3c1153e570cb2890
FaseehHayat21 <faseehhayat999@gmail.com>
2025-01-07 21:16:23 +0500

    update

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
+mmongoose
+.connect(MONGO_URI, { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
 
 // Device Schema and Model
 const deviceSchema = new mongoose.Schema({
